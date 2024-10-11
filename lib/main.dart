0fa68b7bb50b3a4e9520d6f0624789efaa5fb555
Piyush <singhpiyush2604@gmail.com>
2024-10-12 00:04:14 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -23,17 +23,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Testing extends StatefulWidget {
-  const Testing({super.key});
-
-  @override
-  State<Testing> createState() => _TestingState();
-}
-
-class _TestingState extends State<Testing> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
 // https://github.com/sunilvijayan7/GetDoctor
