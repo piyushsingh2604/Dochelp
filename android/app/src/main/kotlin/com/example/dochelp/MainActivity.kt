149984937401de0a89347829647e7c2653a1c4d8
Piyush <singhpiyush2604@gmail.com>
2024-10-11 17:32:12 +0530

    bottombar

---
+++ android/app/src/main/kotlin/com/example/dochelp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dochelp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/UI/Screens/Widgets/BottomBar.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
+
+class BottomBar extends StatefulWidget {
+  @override
+  _BottomBarState createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  final PageController _pageController = PageController();
+  final NotchBottomBarController _notchBottomBarController =
+      NotchBottomBarController();
+  int _currentIndex = 0;
+
+  void _onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+    _pageController.jumpToPage(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        children: [
+          Center(child: Text('Page 1')),
+          Center(child: Text('Page 2')),
+          Center(child: Text('Page 3')),
+        ],
+      ),
+      bottomNavigationBar: AnimatedNotchBottomBar(
+        notchBottomBarController: _notchBottomBarController,
+        bottomBarItems: [
+          BottomBarItem(
+            inActiveItem: Icon(
+              Icons.home_filled,
+              color: Colors.blueGrey,
+            ),
+            activeItem: Icon(
+              Icons.home_filled,
+              color: Colors.blueAccent,
+            ),
+            itemLabel: 'Home',
+          ),
+          BottomBarItem(
+            inActiveItem: Icon(
+              Icons.star,
+              color: Colors.blueGrey,
+            ),
+            activeItem: Icon(
+              Icons.star,
+              color: Colors.blueAccent,
+            ),
+            itemLabel: 'Favorites',
+          ),
+          BottomBarItem(
+            inActiveItem: Icon(
+              Icons.settings,
+              color: Colors.blueGrey,
+            ),
+            activeItem: Icon(
+              Icons.settings,
+              color: Colors.blueAccent,
+            ),
+            itemLabel: 'Settings',
+          ),
+        ],
+        onTap: _onTap,
+        kIconSize: 30.0, // Example icon size
+        kBottomRadius: 25.0, // Example bottom radius
+      ),
+    );
+  }
+}
