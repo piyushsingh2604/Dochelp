f43f31d78b519b3a0c928825f4934d0abddcecaf
Piyush <singhpiyush2604@gmail.com>
2024-10-11 20:00:13 +0530

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:dochelp/UI/Widgets/BottomBar.dart';
+import 'package:dochelp/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
