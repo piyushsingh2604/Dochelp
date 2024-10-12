94311752e663050e43a71af8c111471eff66291c
Piyush <singhpiyush2604@gmail.com>
2024-10-12 21:29:13 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dochelp/UI/Widgets/BottomBar.dart';
+import 'package:dochelp/UI/Widgets/Swiper_Widget.dart';
 import 'package:dochelp/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -20,82 +21,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Test(),
+      home: SwiperWidget(),
     );
   }
 }
 
-class Test extends StatefulWidget {
-  const Test({super.key});
 
-  @override
-  State<Test> createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Doctor List"),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('doctor').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text("Error: ${snapshot.error}"),
-            );
-          } else if (snapshot.hasData) {
-            final docs = snapshot.data!.docs;
-
-            List<Widget> cards = docs.map((doc) {
-              var data = doc.data() as Map<String, dynamic>;
-              return Card(
-                  child: Container(
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 15,
-                      left: 20,
-                      child: Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.green,
-                            borderRadius: BorderRadius.circular(60)),
-                      ),
-                    )
-                  ],
-                ),
-              ));
-            }).toList();
-
-            return Container(
-              height: 190,
-              width: MediaQuery.of(context).size.width,
-              child: CardSwiper(
-                backCardOffset: Offset(15, 15),
-                cardsCount: cards.length,
-                cardBuilder:
-                    (context, index, percentThresholdX, percentThresholdY) =>
-                        cards[index],
-              ),
-            );
-          } else {
-            return Center(
-              child: Text("No data found"),
-            );
-          }
-        },
-      ),
-    );
-  }
-}
 
 
 
